7a436f48f224d7bd1510b132a91a0f86250eb6f7
Hirusha1-cpu <hirushafernando121gmail.com>
2023-02-03 21:33:16 +0530

    Create sign in and forgot password and OAuth component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
@@ -30,7 +31,7 @@ export default function SignIn() {
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
           <form>
             <input
-              className="w-full"
+              
               type="email"
               id="email"
               value={email}
@@ -41,7 +42,7 @@ export default function SignIn() {
             />
             <div className="relative mb-6">
               <input
-                className="w-full"
+                
                 type={showPassword ? "text" : "password"}
                 id="password"
                 value={password}
@@ -71,13 +72,28 @@ export default function SignIn() {
                 >Register</Link>
               </p>
               <p>
-                <Link to="forgot-password" 
-                className="text-blue-600 hover:text-red-700
+                <Link to="/forgot-password" 
+                className="text-blue-600 hover:text-red-800
                 transition duration-200
                 ease-in-out ml-1">Forgot Password</Link>
               </p>
             </div>
+            <button className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium
+          uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg 
+          active:bg-blue-800" type="submit">Sign in</button>
+          <div className="flex item-center` my-4
+          before:border-t before:flex-1
+          before:border-gray-300
+          after:border-t after:flex-1
+          after:border-gray-300">
+
+            <p className="text-center
+            font-semibold mx-4">OR</p>
+          </div>
+          <OAuth/>
           </form>
+          
+          
         </div>
       </div>
     </section>
