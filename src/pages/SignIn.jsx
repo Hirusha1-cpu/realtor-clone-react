1feb628918e4ec5962241d953cc63d94808167be
Hirusha1-cpu <hirushafernando121gmail.com>
2023-02-03 13:20:23 +0530

    create sign in page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,85 @@
-
+import { useState } from "react";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 export default function SignIn() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    Password: "",
+  });
+  const { email, password } = formData;
+  function onChange(e) {
+    console.log(e.target.value);
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
   return (
-    <div>SignIn</div>
-  )
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">Sign in</h1>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img
+            src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            alt="logo"
+            className="w-full rounded-2xl"
+          />
+        </div>
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form>
+            <input
+              className="w-full"
+              type="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email Address"
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300
+            rounded transition ease-in-out"
+            />
+            <div className="relative mb-6">
+              <input
+                className="w-full"
+                type={showPassword ? "text" : "password"}
+                id="password"
+                value={password}
+                onChange={onChange}
+                placeholder="Password"
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300
+            rounded transition ease-in-out"
+              />
+              {showPassword ? (
+                <AiFillEyeInvisible
+                  className="absolute right-3 top-3 text-xl cursor-pointer"
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                />
+              ) : (
+                <AiFillEye
+                  className="absolute right-3 top-3 text-xl cursor-pointer"
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                />
+              )}
+            </div>
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+
+              <p className="mb-6">Don't Have Account?
+                <Link to="/sign-up" className="text-red-600 hover:text-red-700
+                transition duration-200
+                ease-in-out ml-1" 
+                >Register</Link>
+              </p>
+              <p>
+                <Link to="forgot-password" 
+                className="text-blue-600 hover:text-red-700
+                transition duration-200
+                ease-in-out ml-1">Forgot Password</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
